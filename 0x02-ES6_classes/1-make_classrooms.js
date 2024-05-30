460988cd1a2d1fc6fca654d740ef9764e9342080
Mos3ab696 <mos3ab696@gmail.com>
2024-05-30 22:58:07 +0300

    Ta_1_sk

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,7 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
+
 export default function initializeRooms() {
-  let rooms = [];
+  const rooms = [];
   rooms.push(new ClassRoom(19));
   rooms.push(new ClassRoom(20));
   rooms.push(new ClassRoom(34));
