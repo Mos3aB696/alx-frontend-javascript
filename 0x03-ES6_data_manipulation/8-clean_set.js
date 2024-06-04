78eb7b5dce4da9027e5bb988d9ff8eecda9dd339
Mos3ab696 <mos3ab696@gmail.com>
2024-06-04 14:21:07 +0300

    Ta_8_sk

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,15 @@
 export default function cleanSet(set, startString) {
-  if (!set || set.size === 0) return '';
-  if (typeof set !== 'object' || !set instanceof Set) return '';
-  if (startString === '') return '';
+  if (!set || set.size === 0) {
+    return '';
+  }
+  if (startString === '') {
+    return '';
+  }
 
   let result = '';
   for (const item of set) {
     if (typeof item === 'string' && item.startsWith(startString)) {
-      result += item.slice(startString.length) + '-';
+      result += `${item.slice(startString.length)}-`;
     }
   }
 
