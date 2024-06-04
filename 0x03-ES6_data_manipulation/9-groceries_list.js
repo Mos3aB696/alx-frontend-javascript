1c105f7cb90fac5e54b1ce9620c71b0a831ae2ae
Mos3ab696 <mos3ab696@gmail.com>
2024-06-04 14:40:06 +0300

    Ta_9_sk

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,10 @@
 export default function groceriesList() {
-  const groceries = new Map();
-
-  groceries.set('Apples', 10);
-  groceries.set('Tomatoes', 10);
-  groceries.set('Pasta', 1);
-  groceries.set('Rice', 1);
-  groceries.set('Bananas', 5);
-
-  return groceries;
+  const groceries = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(groceries);
 }
